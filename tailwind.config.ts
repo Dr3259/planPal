0783d425ea74578476ccfd39a40ab69478b76d5c
窦瑞 <d1844532477r@gmail.com>
2026-02-07 16:42:31 +0000

    字体的样式细腻

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['"Noto Sans SC"', 'sans-serif'],
+        headline: ['"ZCOOL XiaoWei"', 'serif'],
         code: ['monospace'],
       },
       colors: {
