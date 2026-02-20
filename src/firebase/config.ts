bd4a9009337ba40fe1a3dced1d5611db7ad2722d
窦瑞 <d1844532477r@gmail.com>
2026-02-20 20:49:32 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/con

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyDNRQlqz6GBofanabO6lktiptyXoERkFCw",
   authDomain: "studio-6024501748-4c21c.firebaseapp.com",
   projectId: "studio-6024501748-4c21c",
-  storageBucket: "studio-6024501748-4c21c.appspot.com",
+  storageBucket: "studio-6024501748-4c21c.firebasestorage.app",
   messagingSenderId: "993028368362",
   appId: "1:993028368362:web:0137163aefa293964df493"
 };
